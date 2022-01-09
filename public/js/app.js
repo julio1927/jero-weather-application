a45f3bae3ab2e425cd6220bc21569a76dee40e82
julio1927 <38108470+julio1927@users.noreply.github.com>
2022-01-09 08:31:25 -0500

    app code structure

--- public/js/app.js
+++ public/js/app.js
@@ -5,21 +5,19 @@ const search = document.querySelector("#address");
 const msg1 = document.querySelector("#msg1");
 const msg2 = document.querySelector("#msg2");
 
-msg1.textContent = 'Loading...';
-msg2.textContent = ' ';
-
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  console.log("Im in fetch");
-  
   const location = search.value;
+
+  msg1.textContent = "Loading...";
+  msg2.textContent = " ";
 
   fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         msg1.textContent = data.error;
-        msg2.textContent = ' ';
+        msg2.textContent = " ";
       } else {
         msg1.textContent = data.location;
         msg2.textContent = data.forecast;
